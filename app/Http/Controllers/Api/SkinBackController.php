bebbb3730dd0f410e2f1a567fe2d5a9355b517cd
Mattia Basone <mattia.basone@gmail.com>
2020-04-24 14:56:00 +0200

    wip route separation

--- app/Http/Controllers/Api/SkinBackController.php
+++ app/Http/Controllers/Api/SkinBackController.php
@@ -17,17 +17,17 @@ class SkinBackController extends BaseApiController
      * Serve Avatar.
      *
      * @param \Illuminate\Http\Request
-     * @param string $uuidOrName User UUID or Username
+     * @param string $uuid User UUID
      * @param int    $size
      *
      * @throws \Throwable
      *
      * @return \Illuminate\Http\Response
      */
-    public function serveUuid(Request $request, $uuidOrName, $size = 0): Response
+    public function serveUuid(Request $request, $uuid, $size = 0): Response
     {
         $size = (int) $size;
-        $this->minepic->initialize($uuidOrName);
+        $this->minepic->initialize($uuid);
         $this->minepic->updateStats();
 
         return $this->pngResponse(
@@ -37,17 +37,17 @@ class SkinBackController extends BaseApiController
 
     /**
      * @param Request $request
-     * @param string  $uuidOrName
+     * @param string  $username Username
      * @param int     $size
      *
      * @throws \Throwable
      *
      * @return Response
      */
-    public function serveUsername(Request $request, $uuidOrName, $size = 0): Response
+    public function serveUsername(Request $request, $username, $size = 0): Response
     {
         $size = (int) $size;
-        $this->minepic->initialize($uuidOrName);
+        $this->minepic->initialize($username);
         $this->minepic->updateStats();
 
         return $this->pngResponse(
